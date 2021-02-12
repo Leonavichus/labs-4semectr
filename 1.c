e7e20b8adef2989882f3e470da70bd86c2606b88
Yoshi <70684753+Leonavichus@users.noreply.github.com>
2021-02-12 13:27:41 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -12,11 +12,41 @@
   void printArr(int *M){
     int i;
     for(i=0;i<size;i++)
-    printf("%d\n",M[i]);
+    printf("%d\t",M[i]);
+  }
+
+  void copyArr(int *M, int *K){
+    int i;
+    for(i=0;i<size;i++)
+      K[i]=M[i];
+  }
+  
+  void Bubble(int (*swap)(void),int *M){
+    int i,j,q;
+    for(i=0;i<size-1;i++)
+      for(j=size-1;j>i;j--){
+        if(M[j-1]>M[j]){
+          swap();
+        }
+      }
+  }
+  
+  void swap(int *M,int i,int j,int q){
+          q=M[j];
+          M[j]=M[j-1];
+          M[j-1]=q;
   }
 
   int main(){
+    int i,j,q;
     int A[size],B[size];
     makeArr(A);
     printArr(A);
+    copyArr(A,B);
+    printf("\n");
+    printArr(B);
+    printf("\n");
+    Bubble(swap,B);
+    printArr(B);
+    copyArr(A,B);
   }
