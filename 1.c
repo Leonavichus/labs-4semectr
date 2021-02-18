307665f51b57fe96672cab3bc59dda4702ae24b0
Yoshi <70684753+Leonavichus@users.noreply.github.com>
2021-02-18 20:33:38 +0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -36,6 +36,60 @@ void swap(int *a,int k){
         }
       }
   }
+
+  void Bubble_New(int *M){
+    int i,j = 0,m;
+    do{
+      m=j;
+      for(i=size-1;i>m;i--){
+        if(M[i-1]>M[i]){
+          j=i;
+          swap(M,j);
+        }
+      }
+    } while(j-m);
+  }
+
+  void Shaker(int *M){
+    int i,t,n1=0,n2=size-1,j;
+    do{
+      for(i=n2;i > n1;i--) {
+        if(M[i-1]>M[i]){
+          t=M[i-1];
+          M[i-1]=M[i];
+          M[i]=t;
+          j=i;
+        }
+      }
+    n1=j;
+    for(i=n1;i<=n2;i++){
+      if(M[i-1]>M[i]){
+        t=M[i-1];
+        M[i-1]=M[i];
+        M[i]=t;
+        j=i;
+      }
+    }
+    n2=j-1;
+    } while(n1<n2);
+  }
+
+  void InsertSort(int *M){
+    int i,j,temp;
+    for(i=1;i<size;i++){
+      if(M[i]<M[i-1]){
+        temp=M[i];
+      for(j=i-1;j>=0 && M[j]>temp;j--){
+        M[j+1]=M[j];
+      }
+      M[j+1]=temp;
+      }
+    }
+  }
+
+  void  SelectionSort(int *M){
+    
+  }
   
   int main(){
     int A[size],B[size];
@@ -46,6 +100,22 @@ void swap(int *a,int k){
     printArr(B);
     printf("\n");
     Bubble(B);
+    printArr(B);
+    copyArr(A,B);
+    printf("\n");
+    Bubble_New(B);
+    printArr(B);
+    copyArr(A,B);
+    printf("\n");
+    Shaker(B);
+    printArr(B);
+    copyArr(A,B);
+    printf("\n");
+    InsertSort(B);
+    printArr(B);
+    copyArr(A,B);
+    printf("\n");
+    SelectionSort(B);
     printArr(B);
     copyArr(A,B);
   }
