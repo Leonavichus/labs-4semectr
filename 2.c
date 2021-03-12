df94f1c3e91724fc0e2e7151f357ddd31802ed7e
Yoshi <70684753+Leonavichus@users.noreply.github.com>
2021-03-12 13:19:57 +0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -21,10 +21,51 @@ double Rec_step(int a,int b){
     return Rec_step(a,b-1)*a;
 }
 
+double rec_degree(double a,int b){
+  double r;
+  if(!b)
+    return 1;
+  if(!(b%2))
+    return r=rec_degree(a,b/2), r*r;
+  else 
+    return a*rec_degree(a,b-1);
+}
+
+int NOD_rec(int a,int b){
+  if(!(a%b))
+    return b;
+  else
+    return NOD_rec(b,a%b);
+}
+
+void printd(int k){
+  if(k<0){
+    putchar('-');
+    k= -k;
+  }
+  if(k/10)
+    printd(k/10);
+  putchar(k%10 +'0');
+}
+
+void Reverse(){
+  int ch;
+  if((ch=getch())!='\r'){
+    Reverse();
+    putchar(ch);
+  }
+}
+
+int sum(int *s,int n){
+  if(n==1)
+    return s[0];
+  else
+    return sum(s,n-1) + s[n-1];
+}
+
 int main() {
-  int k = 4,l=7,a=3,b=4;
+  int k = 4,l=7;
   printf("n!=%f\n",Rec_Fact(k));
   printf("fib_l=%lu\n",fib(l));
-  printf("fib_l=%f\n",Rec_step(a,b));
   return 0;
 }
