64f560e3e5d453180546fd2993f508b35a0844b0
Yoshi <70684753+Leonavichus@users.noreply.github.com>
2021-04-08 23:40:17 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -3,13 +3,14 @@
 #include <string.h>
 #include <math.h>
 
-const int N = 2;
+const int N = 5;
 
 typedef struct uroboros{
   char name[10];
   int level;
   int result;
 } gamedev;
+
 
 void enter_arr(gamedev * arr)
 {
@@ -28,22 +29,28 @@ void output_arr(gamedev * arr){
     }
 }
 
-void Insert (gamedev * arr){
-    int i,j, temp;
-    for(i=1;i<N;i++){
-      temp = arr[i].result;
-      j=i-1;
-      while(j>=0 && arr[j].result>temp){
-        arr[j+1].result = arr[j].result;
-        j--;
-      }
-      arr[j+1].result = temp;
+void Select (gamedev * arr){
+    char temp2[100];
+    int i,j,im,temp;
+    for(i=0;i<N-1;i++){
+      im=i;
+      for(j=i+1;j<N;j++){
+        if(arr[j].result<arr[im].result)
+        im=j;}
+        if(i!=im){
+          temp = arr[im].result;
+          arr[im].result = arr[i].result;
+          arr[i].result = temp;
+          strcpy(temp2,arr[i].name);
+          strcpy(arr[i].name,arr[im].name);
+          strcpy(arr[im].name,temp2);
     }
+  }
 }
 
 int main(void){
   gamedev clubs[N];
   enter_arr(clubs);
-  Insert(clubs);
+  Select(clubs);
   output_arr(clubs);
 }
