bc7393a3574daf524ca6951ba594e447298592ae
Yoshi <70684753+Leonavichus@users.noreply.github.com>
2021-04-09 11:51:20 +0300

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+
+typedef struct Node{
+  char val;
+  struct Node *next;
+}NODE,*pNODE;
+
+typedef struct Stack{
+  pNODE top;
+  int len;
+}STACK,*pSTACK;
+
+pSTACK createstack(){
+  pSTACK ps;
+  ps = (pSTACK)malloc(sizeof(STACK));
+    ps->top = NULL;
+    ps->len = 0;
+    return ps;
+}
+
+int isempty(pSTACK ps){
+  if(ps->top) return 0;
+  return 1;
+}
+
+void push(pSTACK ps, char value){
+  pNODE p = (pNODE)malloc(sizeof(NODE));
+    p->val = value;
+    p->next = ps->top;
+    ps->top = p;
+    ps->len++; 
+}
+
+void show(pSTACK ps){
+  pNODE p = ps->top;
+    if(isempty(ps))puts("stack empty");
+    else
+    while(p){
+      printf("%c", p->val);
+      p = p->next;
+    }
+}
+
+void clearstack(pSTACK ps){
+  while(!isempty(ps))
+    pop(ps);
+    free(ps);
+}
+
+char pop(pSTACK ps){
+  pNODE p = ps->top;
+  int c = p->val;
+  ps->top=p->next;
+  free(p);
+  ps->len--;
+  return c;
+}
+
+int tmain(){
+  setlocale(LC_CTYPE,"Russian");
+  pSTACK ps = createstack();
+  char c;
+  for(c='a';c<='z';c++)
+    push(ps,c);
+  while(!isempty(ps)){
+    pop(ps);
+    show(ps);
+    print("\n");
+  }
+  _geth();
+  return 0;
+}
+
+int main(void) {
+  printf("Hello World\n");
+  return 0;
+}
