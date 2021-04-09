a846d4b7c80438c38879a58c809bd4e8076efa50
Yoshi <70684753+Leonavichus@users.noreply.github.com>
2021-04-09 11:39:15 +0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <math.h>
 
-const int N = 5;
+const int N = 3;
 
 typedef struct uroboros{
   char name[10];
@@ -18,20 +18,20 @@ void makeArr(gamedev * arr)
   {
     printf("Введите имя %d команды и результат: ", i+1);
     scanf("%s%d",arr[i].name,&arr[i].result);
-    //printf("Введите уровень: ");
-    //scanf("%d", &arr[i].level);
+    printf("Введите уровень: ");
+    scanf("%d", &arr[i].level);
   }
 }
 
 void printArr(gamedev * arr){
     for (int i = 0; i < N; i++){
-      printf("%d. Команда %s заняла %d место\n",i + 1,arr[i].name,arr[i].result);
+      printf("%d. Команда %s заняла %d место в лиге %d\n",i + 1,arr[i].name,arr[i].result,arr[i].level);
     }
 }
 
 void Select(gamedev * arr){
     char temp2[100];
-    int i,j,im,temp;
+    int i,j,im,temp,temp1;
     for(i=0;i<N-1;i++){
       im=i;
       for(j=i+1;j<N;j++){
@@ -41,11 +41,31 @@ void Select(gamedev * arr){
           temp = arr[im].result;
           arr[im].result = arr[i].result;
           arr[i].result = temp;
+          temp1 = arr[im].level;
+          arr[im].level = arr[i].level;
+          arr[i].level = temp1;
           strcpy(temp2,arr[i].name);
           strcpy(arr[i].name,arr[im].name);
           strcpy(arr[im].name,temp2);
     }
   }
+    for(i=0;i<N-1;i++){
+        im=i;
+        for(j=i+1;j<N;j++){
+          if(arr[j].level<arr[im].level && arr[im].result == arr[j].level)
+          im=j;}
+          if(i!=im){
+            temp = arr[im].result;
+            arr[im].result = arr[i].result;
+            arr[i].result = temp;
+            temp1 = arr[im].level;
+            arr[im].level = arr[i].level;
+            arr[i].level = temp1;
+            strcpy(temp2,arr[i].name);
+            strcpy(arr[i].name,arr[im].name);
+            strcpy(arr[im].name,temp2);
+      }
+    }
 }
 
 int main(void){
